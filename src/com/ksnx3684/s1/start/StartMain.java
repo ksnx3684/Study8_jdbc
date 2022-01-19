af656482e1a58bfb77b932b77dfe847cd9c3cf71
ksnx3684 <ksnx3684@naver.com>
2022-01-19 22:41:55 +0900

    Add Department & Location

--- src/com/ksnx3684/s1/start/StartMain.java
+++ src/com/ksnx3684/s1/start/StartMain.java
@@ -2,9 +2,12 @@ package com.ksnx3684.s1.start;
 
 import java.util.List;
 
-import com.ksnx3684.s1.department.DepartmentDAO;
-import com.ksnx3684.s1.department.DepartmentDTO;
-import com.ksnx3684.s1.department.DepartmentView;
+///import com.ksnx3684.s1.department.DepartmentDAO;
+///import com.ksnx3684.s1.department.DepartmentDTO;
+///import com.ksnx3684.s1.department.DepartmentView;
+import com.ksnx3684.s1.location.LocationDAO;
+import com.ksnx3684.s1.location.LocationDTO;
+import com.ksnx3684.s1.location.LocationView;
 
 public class StartMain {
 
@@ -18,15 +21,25 @@ public class StartMain {
 //			e.printStackTrace();
 //		}
 		
-		DepartmentDAO departmentDAO = new DepartmentDAO();
-		DepartmentView departmentView = new DepartmentView();
+//		DepartmentDAO departmentDAO = new DepartmentDAO();
+//		DepartmentView departmentView = new DepartmentView();
+//		try {
+//			List<DepartmentDTO> ar = departmentDAO.getList();
+//			//departmentDAO.getList();
+//			departmentView.view(ar);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+		
+		LocationDAO locationDAO = new LocationDAO();
+		LocationView locationView = new LocationView();
 		try {
-			List<DepartmentDTO> ar = departmentDAO.getList();
-			//departmentDAO.getList();
-			departmentView.view(ar);
+			List<LocationDTO> ar = locationDAO.getList();
+			locationView.view(ar);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		
 		
 		System.out.println("DB 연동 테스트 종료");
 
